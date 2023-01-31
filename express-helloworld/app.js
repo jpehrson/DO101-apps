dfdc97467f7696869427a252a3471ac2e8aa59f3
jpehrson <68339325+jpehrson@users.noreply.github.com>
2023-01-30 19:22:34 -0700

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,11 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Jared!\n');
+  res.send('Hello Jared!  You are one cool dude.  Cowabunga!\n');
 });
 
-app.get('/mars', function(req, res) {
-  res.send('Hello Mars!\n');
+app.get('/IBM', function(req, res) {
+  res.send('Welcome to Red Hat Open Shift Container Platform provided by IBM!\n');
 });
 
 app.listen(8080, function () {
