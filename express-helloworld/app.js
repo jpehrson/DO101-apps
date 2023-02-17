2261fe4a9eb0c7e3596ac1e97632f9d80bbb5756
jpehrson <68339325+jpehrson@users.noreply.github.com>
2023-02-16 21:31:06 -0700

    PearlJam Changes

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/IBM', function(req, res) {
   res.send('Welcome to Red Hat Open Shift Container Platform provided by IBM!\n');
 });
 
+app.get('/PearlJam', function(req, res) {
+  res.send('Pearl Jam is the best band in the world!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
